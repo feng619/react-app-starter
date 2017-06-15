bd320fe6a0b6c0175c4d485c797c7f423ec7367e
wuchien <fe19ng@gmail.com>
2017-06-15 15:37:57 +0800

    Setup the working environment of Material-UI

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,11 +1,38 @@
 // @flow
 import React, { Component } from 'react';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
 
 class About extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
       <div>
         基本組件切換分頁功能測試用
+        <br />
+        Material-UI 測試
+        <br />
+        <SelectField
+          floatingLabelText="选择项目"
+          value={this.state.value}
+          onChange={(event, index, data) => {
+            this.setState({ value: data });
+          }}
+          style={{
+            width: 140,
+          }}
+        >
+          <MenuItem value={null} primaryText="" />
+          <MenuItem value={1} primaryText="铁轨设施" />
+          <MenuItem value={2} primaryText="油井管线设备" />
+          <MenuItem value={3} primaryText="钢骨结构" />
+        </SelectField>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 // @flow
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
 
-import createHistory from 'history/createBrowserHistory';
+import createHistory from "history/createBrowserHistory";
 
-import { ConnectedRouter, routerMiddleware } from 'react-router-redux';
+import { ConnectedRouter, routerMiddleware } from "react-router-redux";
 
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import injectTapEventPlugin from "react-tap-event-plugin";
 
-import reducers from './reducers';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap-theme.css";
 
-import App from './components/App';
+import reducers from "./reducers";
 
-import registerServiceWorker from './registerServiceWorker';
-import './style/index.css';
+import App from "./components/App";
+
+import registerServiceWorker from "./registerServiceWorker";
+import "./style/index.css";
+
+injectTapEventPlugin();
 
 const history = createHistory();
 const middleware = routerMiddleware(history);
@@ -26,11 +31,13 @@ const store = createStore(reducers, applyMiddleware(middleware));
 
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <App />
-    </ConnectedRouter>
+    <MuiThemeProvider>
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </MuiThemeProvider>
   </Provider>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
 
 registerServiceWorker();
